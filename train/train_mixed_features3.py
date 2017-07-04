dd149b3817947c71802cea3e9d91ab9cbc136c4e
uribernal <oriol.bernal@alu-etsetb.upc.edu>
2017-07-04 17:57:56 +0200

    Added Thesis

--- train/train_mixed_features3.py
+++ train/train_mixed_features3.py
@@ -26,8 +26,10 @@ def get_model(cells=512, bs=1, ts=1, i_dim=7168, dp=0.5, opt=None, lr=1e-8, summ
     input_normalized = BatchNormalization(name='normalization')(input_features)
     input_dropout = Dropout(dp)(input_normalized)
     lstm = LSTM(cells, return_sequences=True, stateful=True, name='lsmt1')(input_dropout)
-    output_dropout = Dropout(dp)(lstm)
-    output = TimeDistributed(Dense(2, activation='sigmoid'), name='fc')(output_dropout)
+    middle_output = Dropout(dp)(lstm)
+    middle_dense = TimeDistributed(Dense(512, activation='tanh'), name='fc1')(middle_output)
+    output_dropout = Dropout(dp)(middle_dense)
+    output = Dense(2, activation='tanh', name='fc')(output_dropout)
 
     model = Model(input=input_features, output=output)
     if opt is None:
@@ -35,7 +37,23 @@ def get_model(cells=512, bs=1, ts=1, i_dim=7168, dp=0.5, opt=None, lr=1e-8, summ
 
     model.compile(loss='mean_squared_error', optimizer=opt)
     print('Model Compiled!')
+    '''
+    input_features = Input(batch_shape=(bs, ts, i_dim,), name='features')
+    input_normalized = BatchNormalization(name='normalization')(input_features)
+    input_dropout = Dropout(dp)(input_normalized)
+    lstm = LSTM(cells, return_sequences=True, stateful=True, name='lsmt1')(input_dropout)
+    middle_dropout = Dropout(dp)(lstm)
+    lstm2 = LSTM(cells//2, return_sequences=True, stateful=True, name='lsmt2')(middle_dropout)
+    output_dropout = Dropout(dp)(lstm2)
+    output = TimeDistributed(Dense(2, activation='tanh'), name='fc')(output_dropout)
 
+    model = Model(input=input_features, output=output)
+    if opt is None:
+        opt = Adam(lr=lr)
+
+    model.compile(loss='mean_squared_error', optimizer=opt)
+    print('Model Compiled!')
+    '''
     '''
         print('Build STATEFUL model...')
         model = Sequential()
@@ -63,20 +81,15 @@ def get_data(split):
     movies = Dm.get_movies_names()
 
     movies_val = [movies[split + 1], movies[split + 2], movies[split + 3]]
-    movies_test = movies[split]
+    movies_test = [movies[split], movies[split + 4]]
+    del movies[split]
     del movies[split]
     del movies[split]
     del movies[split]
     del movies[split]
     movies_train = movies
 
-    with h5py.File(db_path, 'r') as hdf:
-        labels = np.array(hdf.get('dev/labels/' + movies_test))
-        features = np.array(hdf.get('dev/features/' + movies_test))
-    labels_test = labels.reshape(labels.shape[0], 1, labels.shape[1])[:, :, :2]
-    features_test = features.reshape(features.shape[0], 1, features.shape[1])[:, :, :]
-
-    return movies_train, movies_val, features_test, labels_test
+    return movies_train, movies_val, movies_test
 
 
 def train_and_evaluate_model(experiment_id, num_epochs, cells, opt, bs, ts, dp, lr, split):
@@ -88,7 +101,7 @@ def train_and_evaluate_model(experiment_id, num_epochs, cells, opt, bs, ts, dp, 
     model = get_model(cells=cells, bs=bs, ts=ts, dp=dp, opt=opt, lr=lr, summary=True)
 
     # Get the data
-    movies_train, movies_val, x_test, y_test = get_data(split)
+    movies_train, movies_val, movies_test = get_data(split)
 
     print('Train...')
     training_loss_epochs = []
@@ -109,7 +122,7 @@ def train_and_evaluate_model(experiment_id, num_epochs, cells, opt, bs, ts, dp, 
                 tr_loss_movies.append(tr_loss)
             model.reset_states()
 
-        te_loss_movies = []
+        val_loss_movies = []
         for movie in movies_val:
             with h5py.File(db_path, 'r') as hdf:
                 labels = np.array(hdf.get('dev/labels/' + movie))
@@ -119,18 +132,18 @@ def train_and_evaluate_model(experiment_id, num_epochs, cells, opt, bs, ts, dp, 
             for i in range(len(x_val)):
                 x = np.expand_dims(np.expand_dims(x_val[i][0], axis=0), axis=0)
                 y_true = np.array([y_val[i, :, :]])
-                te_loss = model.test_on_batch(x, y_true)
-                te_loss_movies.append(te_loss)
+                val_loss = model.test_on_batch(x, y_true)
+                val_loss_movies.append(val_loss)
             model.reset_states()
 
         training_loss_epochs = np.append(training_loss_epochs, np.mean(tr_loss_movies))
-        test_loss_epochs = np.append(test_loss_epochs, np.mean(te_loss_movies))
+        test_loss_epochs = np.append(test_loss_epochs, np.mean(val_loss_movies))
 
         print('epoch = {}, loss training = {}, loss testing = {}, elapsed time = {}'
-              .format(epoch, np.mean(tr_loss_movies), np.mean(te_loss_movies), time.time()-start))
+              .format(epoch, np.mean(tr_loss_movies), np.mean(val_loss_movies), time.time()-start))
         print('___________________________________')
         Bot.send_message('epoch = {}, loss training = {}, loss testing = {}, elapsed time = {}'
-                         .format(epoch, np.mean(tr_loss_movies), np.mean(te_loss_movies), time.time() - start))
+                         .format(epoch, np.mean(tr_loss_movies), np.mean(val_loss_movies), time.time() - start))
     # Save results
     Bot.save_plots(training_loss_epochs, test_loss_epochs, fig_path)
     model.save('/home/uribernal/PycharmProjects/tfg-2017-oriol.bernal/results/models/model_{}.h5'.format(
@@ -143,34 +156,45 @@ def train_and_evaluate_model(experiment_id, num_epochs, cells, opt, bs, ts, dp, 
     model.save_weights('/home/uribernal/PycharmProjects/tfg-2017-oriol.bernal/results/models/weights_{}.h5'.format(
                 experiment_id))
 
+    valence_mse = np.array([])
+    arousal_mse = np.array([])
+    valence_pcc = np.array([])
+    arousal_pcc = np.array([])
+
     # Predict with de TEST set
-    predicted = model.predict(x_test, batch_size=1)
-    print(predicted)
-    print(predicted.shape)
-    if len(predicted.shape) < 3:
-        # calculate root mean squared error
-        valence_mse = mean_squared_error(predicted[:, 0], y_test[:, 0, 0])
-        print('Valence MSE = {0}\n'.format(valence_mse))
-        arousal_mse = mean_squared_error(predicted[:, 1], y_test[:, 0, 1])
-        print('Arousal MSE = {0}\n'.format(arousal_mse))
+    for movie in movies_test:
+        with h5py.File(db_path, 'r') as hdf:
+            labels = np.array(hdf.get('dev/labels/' + movie))
+            features = np.array(hdf.get('dev/features/' + movie))
+            y_test = labels.reshape(labels.shape[0], 1, labels.shape[1])[:, :, :2]
+        x_test = features.reshape(features.shape[0], 1, features.shape[1])[:, :, :]
 
-        # calculate PCC
-        valence_pcc = compute_pcc(predicted[:, 0], y_test[:, 0, 0])
-        print('Valence PCC = {0}\n'.format(valence_pcc))
-        arousal_pcc = compute_pcc(predicted[:, 1], y_test[:, 0, 1])
-        print('Arousal PCC = {0}\n'.format(arousal_pcc))
-    else:
-        # calculate root mean squared error
-        valence_mse = mean_squared_error(predicted[:, 0, 0], y_test[:, 0, 0])
-        print('Valence MSE = {0}\n'.format(valence_mse))
-        arousal_mse = mean_squared_error(predicted[:, 0, 1], y_test[:, 0, 1])
-        print('Arousal MSE = {0}\n'.format(arousal_mse))
+        predicted = model.predict(x_test, batch_size=1)
 
-        # calculate PCC
-        valence_pcc = compute_pcc(predicted[:, 0, 0], y_test[:, 0, 0])
-        print('Valence PCC = {0}\n'.format(valence_pcc))
-        arousal_pcc = compute_pcc(predicted[:, 0, 1], y_test[:, 0, 1])
-        print('Arousal PCC = {0}\n'.format(arousal_pcc))
+        if len(predicted.shape) < 3:
+            # calculate root mean squared error
+            valence_mse = np.concatenate(valence_mse, mean_squared_error(predicted[:, 0], y_test[:, 0, 0]))
+            print('Valence MSE = {0}\n'.format(valence_mse))
+            arousal_mse = np.concatenate(arousal_mse, mean_squared_error(predicted[:, 1], y_test[:, 0, 1]))
+            print('Arousal MSE = {0}\n'.format(arousal_mse))
+
+            # calculate PCC
+            valence_pcc = np.append(valence_pcc, compute_pcc(predicted[:, 0], y_test[:, 0, 0]))
+            print('Valence PCC = {0}\n'.format(valence_pcc))
+            arousal_pcc = np.append(arousal_pcc, compute_pcc(predicted[:, 1], y_test[:, 0, 1]))
+            print('Arousal PCC = {0}\n'.format(arousal_pcc))
+        else:
+            # calculate root mean squared error
+            valence_mse = np.append(valence_mse, mean_squared_error(predicted[:, 0, 0], y_test[:, 0, 0]))
+            print('Valence MSE = {0}\n'.format(valence_mse))
+            arousal_mse = np.append(arousal_mse, mean_squared_error(predicted[:, 0, 1], y_test[:, 0, 1]))
+            print('Arousal MSE = {0}\n'.format(arousal_mse))
+
+            # calculate PCC
+            valence_pcc = np.append(valence_pcc, compute_pcc(predicted[:, 0, 0], y_test[:, 0, 0]))
+            print('Valence PCC = {0}\n'.format(valence_pcc))
+            arousal_pcc = np.append(arousal_pcc, compute_pcc(predicted[:, 0, 1], y_test[:, 0, 1]))
+            print('Arousal PCC = {0}\n'.format(arousal_pcc))
 
     scores = [np.mean(valence_mse), np.mean(arousal_mse), np.mean(valence_pcc), np.mean(arousal_pcc)]
 
@@ -201,10 +225,4 @@ if __name__ == '__main__':
     dropout = 0.5
     data_split = 0
     # train(epochs, lstm_cells, optimizer, batch_size, timesteps, dropout, data_split=data_split)
-    train(25, 256, None, 1, 1, 0.5, 1e-9, split=0)
-    train(25, 256, None, 1, 1, 0.5, 1e-8, split=0)
-    train(25, 256, None, 1, 1, 0.5, 1e-7, split=0)
-
-    train(25, 2048, None, 1, 1, 0.5, 1e-9, split=0)
-    train(25, 1024, None, 1, 1, 0.5, 1e-9, split=0)
-    train(25, 512, None, 1, 1, 0.5, 1e-9, split=0)
+    train(100, 1024, None, 1, 1, 0.5, 1e-5, split=0)
